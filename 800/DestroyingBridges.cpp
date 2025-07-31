15ed315496a451509c21582698401d5f1f1dec3a
Hari Om <hariomyadv36@gmail.com>
2025-07-31 18:00:20 +0530

    done

---
+++ 800/DestroyingBridges.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;cin>>t;
+    while(t--)
+    {
+        int n,k;
+        cin>>n>>k;
+
+        int maxE = n-1;
+        if(k<maxE) cout<<n<<endl;
+        else cout<<1<<endl;
+    }
+}
